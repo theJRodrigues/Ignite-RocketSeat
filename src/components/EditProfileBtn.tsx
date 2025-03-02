0bbbc0176c210b1f40f4063be87d8363c0b6fed6
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-01 22:21:52 -0300

    font-bold button

--- src/components/EditProfileBtn.tsx
+++ src/components/EditProfileBtn.tsx
@@ -5,7 +5,7 @@ const EditProfileBtn = () => {
   return (
     <button className='flex gap-1 items-center py-[1.4rem] text-green-light px-2.5 border-3 border-green-light rounded-xl cursor-pointer hover:shadow-[0px_0px_13px_-1px_rgba(0,179,126,0.72)] hover:bg-green hover:text-white hover:border-green transition-colors'>
         <PencilLine size={20}/>
-        <span className='text-lg'>Editar seu perfil</span>
+        <span className='text-lg font-bold'>Editar seu perfil</span>
     </button>
   )
 }
